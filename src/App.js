c817d4b131656b0535463b6674a8225c9b3050d6
leetaewoong <bugkingus@gmail.com>
2021-08-06 17:04:34 +0900

    Faild To Compile Profiles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import About from './About';
+import Home from './Home';
+import Profile from './Profile';
+import Profiles from './Profiles';
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+
+        
+        <ul>
+          <li><Link to="/">홈</Link></li>
+          <li><Link to="/about">소개</Link></li>
+          <li><Link to="/profile/velopert">velopert 프로필</Link></li>
+          <li><Link to ="/profile/gildong">gildong 프로필</Link></li>
+        </ul>
+        
+        <Route path="/" exact component={Home} />
+        <Route path={['/about', '/info']} component={About} />        
+        <Route path="/profile/:username" component={Profile} />
+        <Route path="/profiles/:username" component={Profiles} />
+        
+        
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
